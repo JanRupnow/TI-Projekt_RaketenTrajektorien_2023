2052a6414c4a9fe380acdf31257896030977d79c
LouisMeier <louismeier28@gmail.com>
2023-04-22 18:39:57 +0200

    ADD mehr Kommentare

--- animationTest.py
+++ animationTest.py
@@ -7,34 +7,35 @@ fig, ax = plt.subplots()
 #line, = ax.plot(x, y)
 
 
-Abwurfhoehe = 0
-AbwurfWinkel = 0
-KoerperMasse = 100000
-AbwurfGeschwindigkeit = 11000
-Luftwiederstand = 0.0002
-Startzeit = 0
-Endzeit = 100000
+Abwurfhoehe = 0                 # Höhe über dem Meeresspiegel nur in z-Richtung [m]
+AbwurfWinkel = 0                # Winkel [°]
+KoerperMasse = 100000           # Masse des Objekts [kg]
+AbwurfGeschwindigkeit = 11000   # Abwurfsgeschwindigkeit [m/s]
+Luftwiederstand = 0.0002        # Luftwiderstandsbeiwert
+Startzeit = 0                   # [s]
+Endzeit = 100000                # [s]
 Rechenschritte = 100000
-z_speed = 0
-x_speed = 0
-Stop = False
-FallBeschleunigung = 9.81
+z_speed = 0                     # aktuell nicht genutzt     
+x_speed = 0                     #  - Verwendung zur Einstellung des Schubs
+Stop = False                    # Wenn eine Kollision mit der Erde festgestellt wurde, dann deaktiviert diese Variable weitere Berechnungen
+FallBeschleunigung = 9.81       # [m/s^2]
 c = Luftwiederstand/KoerperMasse
 
-G = 6.674 * 10**(-11)   # Gravitationskonstante [Nm^2/kg^2]
-m_E = 5.972 * 10**24    # Erdmasse [kg]
-r_E = 6.37 * 10**6      # Erdradius [m]
+G = 6.674 * 10**(-11)           # Gravitationskonstante [Nm^2/kg^2]
+m_E = 5.972 * 10**24            # Erdmasse [kg]
+r_E = 6.37 * 10**6              # Erdradius [m]
 
-r_x= np.zeros(Rechenschritte)
-r_z= np.zeros(Rechenschritte)
-v_x=np.zeros(Rechenschritte)
-v_z=np.zeros(Rechenschritte)
+r_x= np.zeros(Rechenschritte)   # x-Position [m]
+r_z= np.zeros(Rechenschritte)   # z-Position [m]
+v_x=np.zeros(Rechenschritte)    # x-Geschwindigkeit [m/s]
+v_z=np.zeros(Rechenschritte)    # z-Geschwindigkeit [m/s]
 t=np.linspace(Startzeit, Endzeit, Rechenschritte)
 dt=(Endzeit-Startzeit)/Rechenschritte
 
 AktuellerSchritt = 0
 AktuellerRechenschritt = 0
 
+# Berechnung nach Runge-Kutta Verfahren
 def berechneNaechstenSchritt(i: int):
     global r_x, r_z, v_x, v_z
     # z-Komponente
@@ -55,24 +56,26 @@ def berechneNaechstenSchritt(i: int):
     v_x[i+1] = v_x[i] + k*dt
     r_x[i+1] = r_x[i] + v_x[i]*dt
 
+# Methode für die z-Komponente
 def f1(r_x, r_z, x, t):
     y=-(G*m_E/(r_x**2 + r_z**2)**1.5) * r_z - c*x**2*np.sign(x)
     
+    # Beschleunigung soll nur zum aktuellen Zeitpunkt hinzugefügt werden
+    #  - 'w' beschleunigt nach oben und 's' nach unten
     if AktuellerSchritt == AktuellerRechenschritt:
-        #y = z_speed
-        
         if keyboard.is_pressed("w"):
-            #y = z_speed
-
             y += 10*FallBeschleunigung
         if keyboard.is_pressed("s"):
             y -= 10*FallBeschleunigung
         
     return y
 
+# Methode für die y-Komponente
 def f2(r_x, r_z, x, t):
     y=-(G*m_E/(r_x**2 + r_z**2)**1.5) * r_x - c*x**2*np.sign(x)
     
+    # Beschleunigung soll nur zum aktuellen Zeitpunkt hinzugefügt werden
+    #  - 'd' beschleunigt nach rechts und 'a' nach links
     if AktuellerSchritt == AktuellerRechenschritt:
         if keyboard.is_pressed("d"):
             y += 10*FallBeschleunigung
@@ -81,17 +84,13 @@ def f2(r_x, r_z, x, t):
 
     return y
 
-#def berechneAktuelleExtraKraft():
-""""
-def tastenGedrueckt():
-    if keyboard.is_pressed("a"):
-"""
 # Anfangsbedingungen
 r_x[0] = 0
 r_z[0] = r_E + Abwurfhoehe
 v_x[0] = AbwurfGeschwindigkeit * np.cos(AbwurfWinkel * np.pi / 180)
 v_z[0] = AbwurfGeschwindigkeit * np.sin(AbwurfWinkel * np.pi / 180)
 
+# Die ersten 1000 Schritte berechnen
 for i in range(1000):
     berechneNaechstenSchritt(AktuellerRechenschritt)
     AktuellerRechenschritt += 1
@@ -107,10 +106,13 @@ def animate(i):
                 berechneNaechstenSchritt(AktuellerRechenschritt)
                 AktuellerRechenschritt += 1
 
+        # Wenn die Funktion animate() aufgerufen wird, dann wird der nächste Schritt angezeigt, 
+        # deshalb muss ein weiterer berechnet werden, um immer die nächsten 1000 Schritte anzuzeigen
         else:
             berechneNaechstenSchritt(AktuellerRechenschritt)
             AktuellerRechenschritt += 1
-        # Wenn die Rakete in die Erde fliegt stoppt die Berechnung
+
+        # Wenn die Rakete in die Erde fliegt, dann stoppt die Berechnung
         if np.sqrt(r_x[i+1]**2 + r_z[i+1]**2) <= r_E:
             Stop = True
 
@@ -125,18 +127,18 @@ def animate(i):
         AktuellerSchritt += 1
         # Skalierung der Animation relativ zu Erde
         ax.set_ylim(r_z[AktuellerSchritt]-r_E*5,r_z[AktuellerSchritt]+r_E*5)
-
         ax.set_xlim(left=r_x[AktuellerSchritt]-r_E*5, right=r_x[AktuellerSchritt]+r_E*5)
         # Skalierung des Graphen 
 
-        # Anzeigen von Daten (rechts oben)
-        text.set_text("r_x: {0}\nr_z: {1}\nv_x: {2}\nv_z: {3}\nt: {4}".format(
+        # Anzeigen von Daten (rechts oben) (Position, Geschwindigkeit, Zeit)
+        text.set_text("r_x: {0}m\nr_z: {1}m\nv_x: {2}m/s\nv_z: {3}m/s\nt: {4}s".format(
                             round(r_x[AktuellerSchritt], 2),
                             round(r_z[AktuellerSchritt], 2),
                             round(v_x[AktuellerSchritt], 2), 
                             round(v_z[AktuellerSchritt], 2), 
                             AktuellerSchritt * dt))
         text.set_position(((r_x[AktuellerSchritt]+r_E*5) * 0.8, (r_z[AktuellerSchritt]+r_E*5) * 0.8))
+
     return line, lineBisJetzt, aktuellerPunkt
 
 # Plotten der Erde
@@ -144,11 +146,12 @@ circle1 = plt.Circle((0,0), r_E)
 ax.add_artist(circle1)
 
 
-# Fluglinie
+# Fluglinie (zukünftige in rot)
 line, = ax.plot(r_x[:AktuellerRechenschritt], r_z[:AktuellerRechenschritt], 'r--')
+# Fluglinie (vergangene in blau)
 lineBisJetzt, = ax.plot(r_x[:0], r_z[:0], 'b--')
+# Punkt an der aktuellen Position
 aktuellerPunkt, = ax.plot(r_x[0], r_z[0], 'o')
-#plt.scatter(0, 0, r_E)
 
 # Animationsfunktion
 ani = animation.FuncAnimation(
@@ -160,9 +163,3 @@ text = ax.text(0, 0, '')
 plt.rcParams['keymap.save'].remove('s')
 plt.rcParams["figure.autolayout"] = True
 plt.show()
-
-###Darstellung der Funktion
-""""
-    line = ax.plot(x, Abwurfhoehe, 'r--')[0]  ###Wurfbahn
-    ax.plot(WurfWeite, 0, 'bo')  ###Kontakt mit dem Boden
-"""
