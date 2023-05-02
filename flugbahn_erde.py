23286c597e83aa238ef73f10e26ac4abea2fc540
JanRupnow <jan.rupnow@gmx.de>
2023-05-02 10:05:39 +0200

    Planet Description

--- flugbahn_erde.py
+++ flugbahn_erde.py
@@ -126,7 +126,7 @@ class Rocket:
         self.aktuellerschritt+= 1
         
 class Planet:
-    def __init__(self, x, y, radius, color, mass):
+    def __init__(self, x, y, radius, color, mass,name):
         self.x = x
         self.y = y
         self.radius = radius
@@ -137,6 +137,7 @@ class Planet:
         self.distance_to_sun = 0
         self.x_vel = 0
         self.y_vel = 0
+        self.name = name
 
     def draw(self, window, show, move_x, move_y, draw_line):
         x = self.x * SCALE + WIDTH / 2
@@ -152,7 +153,7 @@ class Planet:
                 pygame.draw.lines(window, self.color, False, updated_points, 1)   
         pygame.draw.circle(window, self.color, (x + move_x, y + move_y), self.radius)
         if not self.sun:
-            distance_text = FONT_2.render(f"{round(self.distance_to_sun * 1.057 * 10 ** -16, 8)} light years", True,
+            distance_text = FONT_2.render(f"{round(self.distance_to_sun * 1.057 * 10 ** -16, 8)} light years to "+self.name, True,
                                           COLOR_WHITE)
             if show:
                 window.blit(distance_text, (x - distance_text.get_width() / 2 + move_x,
@@ -202,31 +203,31 @@ def main():
 
     # Metric from: https://nssdc.gsfc.nasa.gov/planetary/factsheet/
 
-    sun = Planet(0, 0, 30 * SCALE * 10 ** 9, COLOR_SUN, 1.98892 * 10 ** 30)
+    sun = Planet(0, 0, 30 * SCALE * 10 ** 9, COLOR_SUN, 1.98892 * 10 ** 30,"Sonne")
     sun.sun = True
 
-    mercury = Planet(-0.387 * AU, 0, 5 * SCALE * 10 ** 9, COLOR_MERCURY, 3.30 * 10 ** 23)
+    mercury = Planet(-0.387 * AU, 0, 5 * SCALE * 10 ** 9, COLOR_MERCURY, 3.30 * 10 ** 23,"Merkur")
     mercury.y_vel = 47.4 * 1000
 
-    venus = Planet(-0.723 * AU, 0, 9 * SCALE * 10 ** 9, COLOR_VENUS, 4.8685 * 10 ** 24)
+    venus = Planet(-0.723 * AU, 0, 9 * SCALE * 10 ** 9, COLOR_VENUS, 4.8685 * 10 ** 24,"Venus")
     venus.y_vel = 35.02 * 1000
 
-    earth = Planet(-1 * AU, 0, 10 * SCALE * 10 ** 9, COLOR_EARTH, 5.9722 * 10 ** 24)
+    earth = Planet(-1 * AU, 0, 10 * SCALE * 10 ** 9, COLOR_EARTH, 5.9722 * 10 ** 24,"Erde")
     earth.y_vel = 29.783 * 1000
 
-    mars = Planet(-1.524 * AU, 0, 5 * SCALE * 10 ** 9, COLOR_MARS, 6.39 * 10 ** 23)
+    mars = Planet(-1.524 * AU, 0, 5 * SCALE * 10 ** 9, COLOR_MARS, 6.39 * 10 ** 23,"Mars")
     mars.y_vel = 24.077 * 1000
 
-    jupiter = Planet(-5.204 * AU, 0, 20 * SCALE * 10 ** 9, COLOR_JUPITER, 1.898 * 10 ** 27)
+    jupiter = Planet(-5.204 * AU, 0, 20 * SCALE * 10 ** 9, COLOR_JUPITER, 1.898 * 10 ** 27,"Jupiter")
     jupiter.y_vel = 13.06 * 1000
 
-    saturn = Planet(-9.573 * AU, 0, 18 * SCALE * 10 ** 9, COLOR_SATURN, 5.683 * 10 ** 26)
+    saturn = Planet(-9.573 * AU, 0, 18 * SCALE * 10 ** 9, COLOR_SATURN, 5.683 * 10 ** 26,"Saturn")
     saturn.y_vel = 9.68 * 1000
 
-    uranus = Planet(-19.165 * AU, 0, 14 * SCALE * 10 ** 9, COLOR_URANUS, 8.681 * 10 ** 25)
+    uranus = Planet(-19.165 * AU, 0, 14 * SCALE * 10 ** 9, COLOR_URANUS, 8.681 * 10 ** 25,"Uranus")
     uranus.y_vel = 6.80 * 1000
 
-    neptune = Planet(-30.178 * AU, 0, 12 * SCALE * 10 ** 9, COLOR_NEPTUNE, 1.024 * 10 ** 26)
+    neptune = Planet(-30.178 * AU, 0, 12 * SCALE * 10 ** 9, COLOR_NEPTUNE, 1.024 * 10 ** 26,"Neptun")
     neptune.y_vel = 5.43 * 1000
 
     planets = [neptune, uranus, saturn, jupiter, mars, earth, venus, mercury, sun]
