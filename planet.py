ea7c38fadba953765e3f34d3f18ebf2b5802b15c
JanRupnow <jan.rupnow@gmx.de>
2023-05-10 11:18:29 +0200

    Moon Velocity

--- planet.py
+++ planet.py
@@ -11,7 +11,6 @@ class Planet:
         self.mass = mass
         self.orbit = []
         self.sun = False
-        self.distance_to_sun = 0
         self.x_vel = 0
         self.y_vel = 0
         self.name = name
@@ -57,8 +56,6 @@ class Planet:
         distance_x = other_x - self.x
         distance_y = other_y - self.y
         distance = math.sqrt(distance_x ** 2 + distance_y ** 2)
-        if other.sun:
-            self.distance_to_sun = distance
         force = G * self.mass * other.mass / distance ** 2
         theta = math.atan2(distance_y, distance_x)
         force_x = math.cos(theta) * force
