995fa9014ae79656e79d10211066b1b9bd797055
LouisMeier <100848944+LouisMeier@users.noreply.github.com>
2023-05-18 21:03:08 +0200

    program can be closed by pressing esc in start view

--- views/start_view.py
+++ views/start_view.py
@@ -20,7 +20,12 @@ def createUiButton(text, position_x,position_y, manager):
 def initialiseStartUI():
     createUiGameTitleLabel("Spaceflight Simulator", WIDTH*0.45, HEIGHT*0.05, manager)
     createUiButton("Start the Game", WIDTH*0.465, HEIGHT*0.8, manager)
+    createUiButton("Configure the Rocket", WIDTH*0.465, HEIGHT*0.7, manager)
+
     
+# removes all ui elements => no used object_ids
+def clearStartUI():
+    manager.clear_and_reset() 
 
 def showStartUI():
     showGUI = True
@@ -33,7 +38,12 @@ def showStartUI():
 
             if event.type == pg.UI_BUTTON_PRESSED and event.ui_object_id == "StarttheGame_label":
                 showGUI = False
+            if checkKeyDown(event, keys.H_closeWindow[0]):
+                pygame.quit()
+                sys.exit()
             manager.process_events(event)
         manager.update(UI_REFRESH_RATE)
         manager.draw_ui(WINDOW)
         pygame.display.update()
+    
+    clearStartUI()
