e60f3ef781be9d514182408069e72b4440a4cbfa
JanRupnow <jan.rupnow@gmx.de>
2023-05-29 13:52:07 +0200

    refactor package_installen

--- package_installer.py
+++ package_installer.py
@@ -4,9 +4,7 @@ import sys
 def install(package):
     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
-install('numpy')
-install('pygame')
-install('pygame_gui')
-install('datetime')
-install('astropy')
-install('sunpy')
+package_list = ["numpy", "pygame", "pygame_gui", "datetime", "astropy", "sunpy"]
+
+for package in package_list:
+    install(package)
