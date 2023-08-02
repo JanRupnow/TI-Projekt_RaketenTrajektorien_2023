d48355595e030db272ddebe9f5d4d526850c2cf0
JanRupnow <jan.rupnow@gmx.de>
2023-08-02 20:24:34 +0200

    fix: PackageInstaller.py catches exception

--- Methods/PackageInstaller.py
+++ Methods/PackageInstaller.py
@@ -3,26 +3,32 @@ import subprocess
 import sys
 
 
-def install(package):
-    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+def install_requirements():
+    try:
+        with open("requirements.txt", 'r') as file:
+            for line in file:
+                package = line.strip()
+                subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+    except Exception as e:
+        print(f"Please install the requirements.txt manually.\n You have to install these following packages:")
+        with open("requirements.txt", 'r') as file:
+            for line in file:
+                package = line.strip()
+                print(package)
+        print(f"You have to search for 'cmd' in your Windows search")
+        print(f"Command: pip install 'package_name'=='desired_version'")
+        print(f"Example: pip install numpy==1.23.5")
 
 
-def install_all_packages():
+def package_installer():
     json_file = open("./Globals/General.json", "r+")
 
     hotkeys_json = json.load(json_file)
     if not hotkeys_json["downloaded"]:
-        package_list = ["numpy==1.23.5",
-                        "pygame==2.3.0",
-                        "DateTime==5.1",
-                        "astropy==5.2.2",
-                        "sunpy==4.1.5",
-                        "json", ]
-        for package in package_list:
-            install(package)
         # Overwriting package status
         new_json = {"downloaded": True}
         json_file.seek(0)
         json_file.truncate()
         json.dump(new_json, json_file, indent=4, ensure_ascii=False)
         json_file.close()
+    install_requirements()
